a6c23ea11c54cb19a412ab717ca26b6e12f1d39e
Dmitry <bdvlug@gmail.com>
2015-08-22 19:44:49 +0300

    update remapped trait

--- src/Caramel/Controllers/Remapped.php
+++ src/Caramel/Controllers/Remapped.php
@@ -15,16 +15,27 @@ trait Remapped
      */
     public function _remap()
     {
+        $route = singular($this->router->uri->rsegments[1]);
         $http_method = $this->input->method();
-        $route = $this->router->method;
+        $params = array_slice($this->router->uri->rsegments, 2);
+
+        if ( ! is_null($this->input->post('_method')) ) {
+            $http_method = strtolower($this->input->post('_method'));
+        }
+
+        // http://resource/1/edit
+        if ( end($this->router->uri->segments) === 'edit') {
+            $http_method = 'edit';
+        }
+
         $action = "{$http_method}_$route";
 
         if (method_exists($this, $action)) {
-            return $this->$action();
+            return $this->{$action}($params);
         }
 
         if (method_exists($this, $route)) {
-            return $this->$route();
+            return $this->{$route}($params);
         }
 
         redirect(404);
